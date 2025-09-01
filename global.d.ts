be916bf096785523595516fca0115d99d72e8d59
Harsh Gupta <0207.harsh@gmail.com>
2025-09-01 05:39:29 +0530

    Fix: added bot widget

---
+++ global.d.ts
@@ -0,0 +1,19 @@
+declare namespace JSX {
+  interface IntrinsicElements {
+    "df-messenger": React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLElement>,
+      HTMLElement
+    > & {
+      intent?: string;
+      "chat-title"?: string;
+      "agent-id"?: string;
+      "language-code"?: string;
+      minimized?: string;
+    };
+
+    "df-messenger-toggle": React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLElement>,
+      HTMLElement
+    >;
+  }
+}
